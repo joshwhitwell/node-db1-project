08d8939e48954b230d35adcd445e75517b125e69
Joshua Whitwell <josh.whitwell@gmail.com>
2020-11-02 17:06:36 -0500

    added update account by id

--- api/accounts-router.js
+++ api/accounts-router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { update } = require('../data/dbConfig')
 
 const db = require('../data/dbConfig')
 
@@ -11,38 +12,61 @@ const Accounts = {
     getById(id) {
         return db('accounts').where({ id }).first()
     },
-    create(account) {
+    insert(account) {
         return db('accounts').insert(account)
+    },
+    update(id, account) {
+        return db('accounts').where({ id }).update(account)
     }
 }
 
 router.get('/', (req, res) => {
     Accounts.get()
-        .then(data => {
-            res.json(data)
+        .then(accounts => {
+            res.status(200).json(accounts)
         })
         .catch(error => {
-            res.json(error)
+            res.status(500).json({ error: error.message })
         })
 })
 
 router.get('/:id', (req, res) => {
     Accounts.getById(req.params.id)
-        .then(data => {
-            res.status(200).json(data)
+        .then(account => {
+            res.status(200).json(account)
         })
         .catch(error => [
-            res.json(error)
+            res.status(500).json({ error: error.message })
         ])
 })
 
 router.post('/', (req, res) => {
-    Accounts.create(req.body)
-        .then(data => {
-            res.json(data)
+    Accounts.insert(req.body)
+        .then(([id]) => {
+            return Accounts.getById(id)
+        })
+        .then(account => {
+            res.status(201).json(account)
         })
         .catch(error => {
-            res.json(error)
+            res.status(500).json({ error: error.message })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    Accounts.update(req.params.id, req.body)
+        .then(updatedItems => {
+            if (updatedItems === 1) {
+                return Accounts.getById(req.params.id)
+            } else if (updatedItems === 0) {
+                res.status(404).json({ message: `Account with ID ${req.params.id} not found `})
+            }
+        })
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message })
         })
 })
 
